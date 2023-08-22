f998e2ab1d9189fe07b9608d330dae449e3cdb2f
DeeDee <21020277@vnu.edu.vn>
2023-08-23 02:06:49 +0700

    Update render to redirect for avoiding unecessary dialog when reload pages

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+let add = "";
+let modify = "";
 let today_id = 3;
 let work_id = 0;
 
@@ -14,6 +16,15 @@ let today_tasks_list = [
 ];
 
 let work_tasks_list = [];
+
+function addTask(list, new_name, counter) {
+    if (new_name == "") {
+        return "fail";
+    } else {
+        list.push({id: counter, name: new_name, description: ""});
+    }
+    return "success";
+}
 
 function modifyTask(list, id, new_name, new_descr) {
     if (id == "") {
@@ -43,23 +54,22 @@ app.listen(port, () => {
 Render today page
 */
 app.get("/", (req, res) => {
-    res.render("index.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: "", add: "" });
+    res.render("index.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: modify, add: add });
+    modify = "";
+    add = "";
 });
 
 app.post("/task-added", (req, res) => {
-    let add = "success";
-    if (req.body["task-name"] == "") {
-        add = "fail";
-    } else {
-        today_tasks_list.push({id: today_id, name: req.body["task-name"], description: ""});
-        today_id++;
-    }
-    res.render("index.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: "", add: add });
+    modify = "";
+    add = addTask(today_tasks_list, req.body["task-name"], today_id);
+    today_id++;
+    res.redirect("/");
 });
 
 app.post("/modify", (req, res) => {
-    let modify = modifyTask(today_tasks_list, req.body["current-id"], req.body["new-name"], req.body["description"]);
-    res.render("index.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: modify, add: "" });
+    add = "";
+    modify = modifyTask(today_tasks_list, req.body["current-id"], req.body["new-name"], req.body["description"]);
+    res.redirect("/");
 });
 
 app.post("/deleted", (req, res) => {
@@ -68,7 +78,7 @@ app.post("/deleted", (req, res) => {
             return e.id != req.body["current-id"];
         });
     }
-    res.render("index.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: "", add: "" });
+    res.redirect("/");
 });
 
 
@@ -76,23 +86,22 @@ app.post("/deleted", (req, res) => {
 Render work page
 */
 app.get("/work", (req, res) => {
-    res.render("work.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: "", add: "" });
+    res.render("work.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: modify, add: add });
+    modify = "";
+    add = "";
 })
 
 app.post("/work/task-added", (req, res) => {
-    let add = "success";
-    if (req.body["task-name"] == "") {
-        add = "fail";
-    } else {
-        work_tasks_list.push({id: work_id, name: req.body["task-name"], description: ""});
-        work_id++;
-    }
-    res.render("work.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: "", add: add });
+    modify = "";
+    add = addTask(work_tasks_list, req.body["task-name"], work_id);
+    work_id++;
+    res.redirect("/work");
 });
 
 app.post("/work/modify", (req, res) => {
-    let modify = modifyTask(work_tasks_list, req.body["current-id"], req.body["new-name"], req.body["description"]);
-    res.render("work.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: modify, add: "" });
+    add = "";
+    modify = modifyTask(work_tasks_list, req.body["current-id"], req.body["new-name"], req.body["description"]);
+    res.redirect("/work");
 });
 
 app.post("/work/deleted", (req, res) => {
@@ -101,5 +110,5 @@ app.post("/work/deleted", (req, res) => {
             return e.id != req.body["current-id"];
         });
     }
-    res.render("work.ejs", { today_tasks_list: today_tasks_list, work_tasks_list: work_tasks_list, modify: "", add: "" });
+    res.redirect("/work");
 });
